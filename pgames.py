a77bb16e7e3db309ae3e8299fb6516856e612a23
armandpizdoff <daveton@yandex.ru>
2025-02-27 21:33:52 +0300

    Update pgames.py

--- pgames.py
+++ pgames.py
@@ -17,7 +17,7 @@ def help_message(message):
 @bot.message_handler(commands=['games'])
 def start_message(message):
     markup = telebot.types.InlineKeyboardMarkup()
-    button1 = telebot.types.InlineKeyboardButton(text='Весёлое кнутирование ;',
+    button1 = telebot.types.InlineKeyboardButton(text='Весёлое кнутирование ;)',
                                                  url='https://t.me/Pizdoff_bot?game=Fun_and_whip')
     markup.row(button1)
     bot.send_message(message.chat.id, text="Приветик, {0.first_name}. Выбери игру!"
